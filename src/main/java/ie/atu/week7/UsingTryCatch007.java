548c911f736675057924c4ee9f524883a075c1bc
Parthibg00410769 <g00410769@atu.ie>
2022-11-08 14:13:24 +0000

    final commit

--- src/main/java/ie/atu/week7/UsingTryCatch007.java
+++ src/main/java/ie/atu/week7/UsingTryCatch007.java
@@ -1,21 +1,36 @@
 package ie.atu.week7;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class UsingTryCatch007 {
     public static void main(String[] args) {
-        System.out.println("Please enter a number between 0-9: ");
+
         Scanner myscan = new Scanner(System.in);
-        int myNum = myscan.nextInt();
-        if (myNum <= 9 && myNum > 0){
+        char Y = 0;
+        while (Y==0){
+            try {
+                System.out.println("Please enter a number between 0-9: ");
+                int myNum = myscan.nextInt();
+                if (myNum <= 9 && myNum > 0) {
 
-            System.out.println("You entered " + myNum);
+                    System.out.println("You entered " + myNum);
+                    break;
+
+
+                }
+            }catch (InputMismatchException e) {
+                System.out.println("Not a valid number!!");
+                System.out.println(e);
+                myscan.next();
+
+
+
+            }
+
         }
-        else{
 
-            System.out.println("Not a valid number");
 
-        }
     }
 
 
